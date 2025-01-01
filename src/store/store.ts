96db72be77e350fe68c61b6d9cf74cc3c9653ba9
Robertstar2000 <MIFECOINC@gmail.com>
2025-01-01 18:04:56 -0500

    fix: add auth reducer to Redux store to fix production error

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import researchReducer from './slices/researchSlice';
+import authReducer from './slices/authSlice';
 
 export const store = configureStore({
   reducer: {
     research: researchReducer,
+    auth: authReducer,
   },
 });
 
